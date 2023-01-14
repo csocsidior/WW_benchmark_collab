b138963681057545972798076829672485833694
Krisztian Balla <cballa7@gmail.com>
2023-01-14 23:04:35 +0100

    Updated gitignore

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clear all;
 
 %% Simulate
 
-[swmm,settings] = simulation.setup(SWMM, 'swmm_files\Astlingen_SWMM.INP', 'ON/OFF', 10*24*3600);
+[swmm,settings] = simulation.setup(SWMM, 'swmm_files\Astlingen_SWMM.INP', 'ON/OFF', 3*24*3600);
 
 [sim_res, settings] = simulation.run_simulation(swmm,settings);
 
